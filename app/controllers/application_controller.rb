33ff7102e897a7247e0cb6de787df8515cc3fed4
N-Viet-ruby-dev <37279359+N-Viet-ruby-dev@users.noreply.github.com>
2019-08-29 09:06:31 +0700

    show_edit_users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :danger, :warning, :info
   include SessionsHelper
+
+  private
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    redirect_to root_path, danger: "Please log in."
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       else
         forget @user
       end
-      	redirect_to @user
+      redirect_back_or @user
     else
       respond_to do |format|
         format.js { flash.now[:danger] = "Invalid email/password combination" }
