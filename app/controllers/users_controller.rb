7f7ee8b82c7cd1a1756e9fe9ef3ee2624829c99a
Hau Nguyen <haunttpd00207@gmail.com>
2019-09-03 10:36:54 +0700

    login_remember

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new user_params
     if @user.save
+      log_in @user
       redirect_to @user, success: "Sign up successfully!"
     else
       respond_to do |format|
